5bab0bbd5b462915788292190ef77e3d947dbec6
Prismoid <seike.hirotsugu@icloud.com>
2017-11-07 04:28:25 +0900

    extend avl-tree function

--- js-RPC/test.js
+++ js-RPC/test.js
@@ -8,6 +8,8 @@ var Web3 = require('web3');
 var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 web3.eth.defaultAccount = web3.eth.accounts[1];
+// AVLTreeのクラス定義
+var AVLTree = require("avl"); // install, https://www.npmjs.com/package/avl
 
 // コントラクトのインスタンスを作成
 var contract_json = require('../build/contracts/Test.json');
@@ -15,10 +17,41 @@ var addr = contract_json.networks[1].address;
 var abi = contract_json.abi;
 var test = web3.eth.contract(abi).at(addr);
 
+
+var keyTgt = new BigNumber("0x1001000000001e33333333", 16); // 56bit(TLDc + SLDc) + 32bit(target)
+// var div = new BigNumber("0x10000000", 16);
+// var tmp = key.divToInt(div);
+// console.log(key);
+// console.log(tmp);
+
+var t = new AVLTree();
+
+var keyInput = new BigNumber("0x1001000000001e00000000ffffffff", 16);
+var strInput = "Hello";
+var n = t.insert(keyInput, strInput);
+
+var n4 = t.find64(keyTgt);
+console.log("ROOT");
+console.log(n4);
+console.log("TEST");
+
+// console.log(t.keys());
+
+
 var event = test.Set();
+test.Set({}, { fromBlock: 0, toBlock: 'latest' }).get((error, results) => {
+    console.log(results[0]);
+    console.log(results[1]);
+});
 //イベント監視
 event.watch(function (error, result) {
  console.log('watching "Set" event!');
   if (!error)
     console.log(result);
 });
+
+/*
+getEvent.get((error, results) => {
+    console.log(JSON.stringify(results));
+});
+*/
